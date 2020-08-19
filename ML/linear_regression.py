2367ccbab27b2ae070940b0cf3599198ced95c7c
Pinaki Mukhopadhyay <mukhopadhyaypinaki@gmail.com>
2020-08-19 22:30:41 +0200

    'in Progress'

--- ML/linear_regression.py
+++ ML/linear_regression.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
 
-
+# Training dataset
 dftrain = pd.read_csv(
-    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')  # Training dataset
+    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')
 
+# Testing dataset
 dfeval = pd.read_csv(
-    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')  # Testing dataset
+    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
 
-
+# remove survived column form the dataframe as label
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
-
-print(dftrain.info())
+#print(dftrain.loc[0], y_train.loc[0])
