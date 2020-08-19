0014895aefd31cb6673d4c2b4e24e9a684d4c045
Pinaki Mukhopadhyay <mukhopadhyaypinaki@gmail.com>
2020-08-19 22:04:15 +0200

    all imports added

--- ML/linear_regression.py
+++ ML/linear_regression.py
@@ -1,12 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from six.moves import urllib
+import tensorflow.compat.v2.feature_column as fc
 
 
 dftrain = pd.read_csv(
-    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')  # Training dataset
 
 dfeval = pd.read_csv(
-    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')  # Testing dataset
+
 
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
