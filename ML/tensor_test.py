615935da4aa36999d6aee61684f8456732c2cebd
Pinaki Mukhopadhyay <mukhopadhyaypinaki@gmail.com>
2020-08-19 21:50:16 +0200

    ploting

--- ML/tensor_test.py
+++ ML/tensor_test.py
@@ -42,3 +42,15 @@ tensor2 = tf.reshape(tensor1, [2, 3, 1])
 print(tensor2)
 
 print(tf.rank(tensor2))
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1, 2, 2.5, 3, 4]
+y = [1, 4, 7, 9, 15]
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+
+plt.show()
