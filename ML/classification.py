474cf82d720bec046aa96d7fb026f5413019cf64
Pinaki Mukhopadhyay <mukhopadhyaypinaki@gmail.com>
2020-08-20 22:24:12 +0200

    classification

---
+++ ML/classification.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from six.moves import urllib
+import tensorflow.compat.v2.feature_column as fc
+import os
+def clear(): return os.system('cls')
